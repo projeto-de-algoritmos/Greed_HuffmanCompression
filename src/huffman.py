1147899d03e75098d0c03e4630d3cc22db9eab9e
Welison Almeida <welison.almeida.923@gmail.com>
2019-10-12 17:16:46 -0300

    Generate tree and codes from Huffman's Algorithm.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

--- src/huffman.py
+++ src/huffman.py
@@ -19,3 +19,33 @@ class Huffman(object):
 
     def compute_histogram(self):
         self.hist = cv2.calcHist([self.img_array], [0], None, [256], [0, 256])
+
+    def generate_huffman_tree(self):
+        nodes = []
+        for i, frequency in enumerate(self.hist):
+            if int(frequency) != 0:
+                node = Node(i, int(frequency))
+                nodes.append(node)
+        heapify(nodes)
+        while len(nodes) > 1:
+            first = heappop(nodes)
+            second = heappop(nodes)
+            new_node = Node(None, first.frequency+second.frequency)
+            new_node.set_children(first, second)
+            heappush(nodes, new_node)
+        
+        self.tree = nodes[0]
+    
+    def generate_huffman_codes(self, code, node):
+        if node == None:
+            return
+        if node.color != None:
+            if not code:
+                self.codes[node.color] = '0'
+                self.rev_codes['0'] = node.color
+            else:
+                self.codes[node.color] = code
+                self.rev_codes[code] = node.color
+        else:
+            self.generate_huffman_codes(code+'0', node.left_node)
+            self.generate_huffman_codes(code+'1', node.right_node)
